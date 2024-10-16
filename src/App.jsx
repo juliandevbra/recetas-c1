940ad29de61e303aa04b704f7356a24b7e5dcd78
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-10-16 03:22:14 -0300

    Clase 2: Creando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Articulo from "./Articulo";
+
+function App() {
+  return (
+    <>
+      <h1>Hola Camada 1</h1>
+      <p>Mi primer proyecto de Vite</p>
+      <ul>
+        <li>Elemento 1</li>
+        <li>Elemento 2</li>
+        <li>Elemento 3</li>
+        <li>Elemento 4</li>
+        <li>Elemento 5</li>
+      </ul>
+      <Articulo />
+    </>
+  );
+}
+
+export default App;
